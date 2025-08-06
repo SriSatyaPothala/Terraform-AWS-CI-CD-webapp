bf168782c3a13276ed5d3509d92c39b7111df1f9
SriSatyaPothala <srisatyapothala11@gmail.com>
2025-08-06 18:39:05 +0530

    changed module name in /envs/dev main.tf

--- terraform-infra/envs/dev/main.tf
+++ terraform-infra/envs/dev/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
 }
 
-module "security_group" {
+module "security_groups" {
   source      = "../../modules/security-groups"
   vpc_id      = module.vpc.vpc_id
   environment = var.environment
