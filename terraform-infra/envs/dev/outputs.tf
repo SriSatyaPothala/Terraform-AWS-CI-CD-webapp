a77c4f47a0c133b7cf42e27c3ee64086e2d49b62
SriSatyaPothala <srisatyapothala11@gmail.com>
2025-08-06 18:53:37 +0530

    Modify outputs.tf in /envs/dev

--- terraform-infra/envs/dev/outputs.tf
+++ terraform-infra/envs/dev/outputs.tf
@@ -21,24 +21,12 @@ output "public_route_table_id" {
 output "private_route_table_ids" {
   value = module.vpc.private_route_table_ids
 }
-
-output "codebuild_role_arn" {
-  value = module.iam.codebuild_role_arn
-}
-
-output "codepipeline_role_arn" {
-  value = module.iam.codepipeline_role_arn
-}
-
-output "codedeploy_role_arn" {
-  value = module.iam.codedeploy_role_arn
-}
 output "alb_sg_id" {
-  value = module.security_group.alb_sg_id
+  value = module.security_groups.alb_sg_id
 }
 
 output "ec2_sg_id" {
-  value = module.security_group.ec2_sg_id
+  value = module.security_groups.ec2_sg_id
 }
 output "alb_dns_name" {
   value = module.alb.alb_dns_name
@@ -48,12 +36,6 @@ output "alb_target_group_arn" {
   value = module.alb.alb_target_group_arn
 }
 
-output "ecr_repository_url" {
-  value = module.ecr.repository_url
-}
-output "artifact_bucket_name" {
-  value = module.s3.artifact_bucket_name
-}
 output "cloudwatch_alb_alarm" {
   value = module.cloudwatch.alb_5xx_alarm_name
 }
