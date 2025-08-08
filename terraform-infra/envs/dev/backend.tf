39b0b7e721df7e59ca5b0e1b4fd1f4f15da76524
SriSatyaPothala <srisatyapothala11@gmail.com>
2025-08-08 12:49:21 +0530

    removed backend.tf

--- terraform-infra/envs/dev/backend.tf
+++ terraform-infra/envs/dev/backend.tf
@@ -1,10 +1,4 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-devopsar25"
-    key            = "dev/terraform.tfstate"   #path for dev 
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-    kms_key_id     = "arn:aws:kms:ap-south-1:345594588323:key/ad6ce9d5-b2d4-48eb-bf88-f40e2e50cf2b"
   }
 }
