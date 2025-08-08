b8805280db19fb1dd7ae7882ce52941b2621ea51
SriSatyaPothala <srisatyapothala11@gmail.com>
2025-08-08 09:28:38 +0530

    Checking the exit codes of docker pull to debug the script failure issue

--- aws-devops/scripts/pull-image.sh
+++ aws-devops/scripts/pull-image.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
+set -e
 
 REGION="ap-south-1"
 echo "Starting pull-image.sh script..."
 IMAGE_TAG=$(cat /home/ubuntu/app/nginx-app/imageTag.txt)
 echo "Image tag from file is: $IMAGE_TAG"
 
-
 ECR_REPO_URI="$(aws ssm get-parameter --name "/dev/ecr_repo_url" --with-decryption --region "$REGION" --query 'Parameter.Value' --output text)"
-# Exit if the ECR URI variable is empty
 if [ -z "$ECR_REPO_URI" ]; then
     echo "ERROR: ECR_REPO_URI variable is empty. Failed to retrieve from SSM."
     exit 1
 fi
 echo "ECR URI from SSM is: $ECR_REPO_URI"
 
-# Authenticate to ECR
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REPO_URI || {
-    echo "ECR Login failed"; exit 1;
-}
-echo "ecr login succeeded."
-
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$ECR_REPO_URI"
+# Capture the exit code of the docker login command
+LOGIN_EXIT_CODE=$?
+if [ $LOGIN_EXIT_CODE -ne 0 ]; then
+  echo "ERROR: Docker login failed with exit code $LOGIN_EXIT_CODE"
+  exit 1
+fi
+echo "Docker login succeeded."
 
 # Pull latest image
-docker pull "$ECR_REPO_URI:$IMAGE_TAG" || {echo "Docker Pull failed for image: $ECR_REPO_URI:$IMAGE_TAG"; exit 1; 
-}
-echo "ecr pull succeeded."
+docker pull "$ECR_REPO_URI:$IMAGE_TAG"
+# Capture the exit code of the docker pull command
+PULL_EXIT_CODE=$?
+if [ $PULL_EXIT_CODE -ne 0 ]; then
+  echo "ERROR: Docker Pull failed with exit code $PULL_EXIT_CODE for image: $ECR_REPO_URI:$IMAGE_TAG"
+  exit 1
+fi
+echo "Docker pull succeeded."
 
 echo "Script completed successfully."
 exit 0
